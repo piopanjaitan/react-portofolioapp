13e41c31cd852fc789bc7b57b57106a8b63c9593
piopanjaitan <pio.panjaitan@gmail.com>
2020-11-21 14:34:53 +0700

    rm import Navbar from LayoutNavbar

--- src/components/LayoutNavbar.js
+++ src/components/LayoutNavbar.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import '../App.css';
 
-import Navbar from './Navbar';
-
 import About from "../sections/About"
 import Experience from "../sections/Experience";
 import Education from "../sections/Education";
